c48e2e60d6e98f703274ab493f458b7aad8bfaf9
Mija Cristian <mija.cristian3101@gmail.com>
2025-09-22 16:22:35 +0300

    New example commit class

--- src/PrimulExempluMetode.java
+++ src/PrimulExempluMetode.java
@@ -1,2 +1,19 @@
 public class PrimulExempluMetode {
+    public static void main(String[] args) {
+        PrimulExempluMetode obiect = new PrimulExempluMetode();
+        arataMesaj();
+        obiect.arataMesaj2();
+    }
+
+    public static void arataMesaj() {
+
+        System.out.println("Mesaj de felicitare");
+
+    }
+
+    public  void arataMesaj2() {
+
+        System.out.println("Mesaj de felicitare");
+
+    }
 }
